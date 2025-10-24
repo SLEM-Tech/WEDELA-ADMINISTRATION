61ad2b72a40753852648387fc4aee1e4bdbfac05
Christopher <ChristopherOhwavworua@transalliance.ng>
2025-10-24 19:28:10 +0100

    updates

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,9 @@
+//@ts-nocheck
+import React from "react";
 import { ReactNode } from "react";
 import Header from "./Navbars/Header";
 import Footer from "./Footers/Footer";
+
 
 interface AppLayoutProps {
 	children: ReactNode;
